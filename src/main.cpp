8a2f66f7928cba3f982a6485e73ff93e906f3d26
Doszhan <doszhan.m@mail.ru>
2022-01-27 23:23:07 +0600

    add webserver

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,17 @@
 #include <ESP8266HTTPClient.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
+#include <WiFiClient.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
-//настройки точки доступа
+// настройки точки доступа
 const char *ssid = "actuator";            // имя точки доступа
 const char *password = "64(Me4J6#C!gZfj"; // пароль точки доступа
+ 
+// настройки wifi подключения
+const char* wifi_ssid = "ASUS_ROUTER";
+const char* wifi_password = "aSus2020";
 
 const char *url = "http://192.168.4.2:8081/zeroconf/switch";
 
@@ -20,12 +27,41 @@ int min_temp = 25;
 bool valve_is_opened = true;
 
 DHT dht(DHT_PIN, DHT_TYPE);       // обьявляем объект dht, указывать тип и дата пин 
-WiFiClient client;                // объявить объект класса
-HTTPClient http;                  // объявить объект класса
+WiFiClient client;                // объявить объект класса wifi
+HTTPClient restclient;            // объявить объект класса rest клиента
+AsyncWebServer server(80);        // объявить объект класса http сервера
+
+// html данные для отрисовки страницы
+const char* PARAM_INPUT_1 = "input1";
+const char* PARAM_INPUT_2 = "input2";
+const char* PARAM_INPUT_3 = "input3";
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+  <title>ESP Input Form</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head><body>
+  <form action="/get">
+    input1: <input type="text" name="input1">
+    <input type="submit" value="Submit">
+  </form><br>
+  <form action="/get">
+    input2: <input type="text" name="input2">
+    <input type="submit" value="Submit">
+  </form><br>
+  <form action="/get">
+    input3: <input type="text" name="input3">
+    <input type="submit" value="Submit">
+  </form>
+</body></html>)rawliteral";
 
 //настройки таймера задержки
 unsigned long lastTime = 0;
 unsigned long timerDelay = 5000; // 5 seconds (5000)
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
+}
 
 void setup()
 {
@@ -42,16 +78,70 @@ void setup()
   digitalWrite (DHT_VCC, HIGH);    // подать напряжение 3,3V на D6 пин
   dht.begin();                     // инициализация DHT22 3
 
-  http.begin(client, url);         // инициализация http клиента
+  WiFi.begin(wifi_ssid, wifi_password);  // подключение к wifi сети
+  Serial.println("WIFI connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Failed!");
+    return;
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
 
-  delay(5000);                       // для стабильности 
+  restclient.begin(client, url);         // инициализация http клиента
+
+
+
+// Send web page with input fields to client
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html);
+  });
+
+  // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage;
+    String inputParam;
+    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+    if (request->hasParam(PARAM_INPUT_1)) {
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();
+      inputParam = PARAM_INPUT_1;
+    }
+    // GET input2 value on <ESP_IP>/get?input2=<inputMessage>
+    else if (request->hasParam(PARAM_INPUT_2)) {
+      inputMessage = request->getParam(PARAM_INPUT_2)->value();
+      inputParam = PARAM_INPUT_2;
+    }
+    // GET input3 value on <ESP_IP>/get?input3=<inputMessage>
+    else if (request->hasParam(PARAM_INPUT_3)) {
+      inputMessage = request->getParam(PARAM_INPUT_3)->value();
+      inputParam = PARAM_INPUT_3;
+    }
+    else {
+      inputMessage = "No message sent";
+      inputParam = "none";
+    }
+    Serial.println(inputMessage);
+    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" 
+                                     + inputParam + ") with value: " + inputMessage +
+                                     "<br><a href=\"/\">Return to Home Page</a>");
+  });
+  server.onNotFound(notFound);
+  server.begin();
+
+
+
+  delay(5000);                           // для стабильности 
 }
 
 void close_valve () {
   // отправить запрос на включение реле
-  http.addHeader("Content-Type", "application/json");                                  // header
+  restclient.addHeader("Content-Type", "application/json");                                  // header
   String RequestData = "{\"deviceid\":\"1000b91ec6\",\"data\":{\"switch\": \"on\"}}"; // payload
-  int ResponseStatusCode = http.POST(RequestData);                                     // post запрос
+  int ResponseStatusCode = restclient.POST(RequestData);                                     // post запрос
   if (ResponseStatusCode == 200)
     {
       Serial.print("Relay has been switched on! Status: ");
@@ -68,9 +158,9 @@ void close_valve () {
 
 void open_valve () {
   // отправить запрос на выключение реле
-  http.addHeader("Content-Type", "application/json");                                  // header
+  restclient.addHeader("Content-Type", "application/json");                                  // header
   String RequestData = "{\"deviceid\":\"1000b91ec6\",\"data\":{\"switch\": \"off\"}}"; // payload
-  int ResponseStatusCode = http.POST(RequestData);                                     // post запрос
+  int ResponseStatusCode = restclient.POST(RequestData);                                     // post запрос
   if (ResponseStatusCode == 200)
     {
       Serial.print("Relay has been switched off! Status: ");
@@ -99,7 +189,7 @@ void loop()
         open_valve();
     }
 
-    http.end(); // Free resources
+    restclient.end(); // Free resources
     lastTime = millis();
   }
 }
