4fc75a0573ce9747db038926b81d8512d0aee994
Doszhan <doszhan.m@mail.ru>
2022-02-07 22:36:00 +0600

    get hummidity

--- data/index.js
+++ data/index.js
@@ -4,13 +4,27 @@ menuToggle.onclick = function () {
   menuToggle.classList.toggle('manual_control_on')
 }
 
+
 setInterval(function () {
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("temperature").innerHTML = this.responseText;
+      console.log('get_temperature')
     }
   };
-  xhttp.open("GET", "/temperature", true);
+  xhttp.open("GET", "/get_temperature", true);
+  xhttp.send();
+}, 20000);
+
+setInterval(function () {
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("humidity").innerHTML = this.responseText;
+      console.log('get_humidity')
+    }
+  };
+  xhttp.open("GET", "/get_humidity", true);
   xhttp.send();
 }, 20000);
--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,59 @@
+#include <Arduino.h>
+
 #include <ESP8266WiFi.h>
+
 #include <ESP8266HTTPClient.h>
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <WiFiClient.h>
+
+#include <DHTNew.h>
+
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+
+#include <FS.h>
 #include "LittleFS.h"
 
 
-// настройки точки доступа
-const char *ssid = "actuator";            // имя точки доступа
-const char *password = "64(Me4J6#C!gZfj"; // пароль точки доступа
- 
-// настройки wifi подключения
+// Конфигурация WIFI подключения
 const char* wifi_ssid = "ASUS_ROUTER";
 const char* wifi_password = "aSus2020";
 
-const char *url = "http://192.168.4.2:8081/zeroconf/switch";
+// настройки точки доступа
+const char *ssid = "actuator";            
+const char *password = "64(Me4J6#C!gZfj"; 
 
 // ----------------------- ПИНЫ ---------------------------
-#define DHT_TYPE DHT22            //тип датчика DHT22
-#define DHT_VCC D6                //питание датчика DHT22
+#define DHT_VCC D6                // питание датчика DHT22
 #define DHT_PIN D5                // дата линия датчика DHT22
 
-int max_temp = 26;
-int min_temp = 25;
 
-bool valve_is_opened = true;
+DHT dht(DHT_PIN, DHT_MODEL_DHT22);  // обьявляем объект класса dht
+AsyncWebServer server(80);          // объявить объект класса http сервера
+
+
+// Переменные
 String valveState = "OPEN";
-
-DHT dht(DHT_PIN, DHT_TYPE);       // обьявляем объект dht, указывать тип и дата пин 
-WiFiClient client;                // объявить объект класса wifi
-HTTPClient restclient;            // объявить объект класса rest клиента
-AsyncWebServer server(80);        // объявить объект класса http сервера
-
-// html данные для отрисовки страницы
-const char* PARAM_INPUT_1 = "input1";
-const char* PARAM_INPUT_2 = "input2";
-const char* PARAM_INPUT_3 = "input3";
-
 
 
 //настройки таймера задержки
 unsigned long lastTime = 0;
 unsigned long timerDelay = 5000; // 5 seconds (5000)
 
+
+// Функции для http сервера ----------------
 void notFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", "Not found");
 }
 
 String getTemperature() {
+  delay(dht.getMinimumSamplingPeriod());
   float temperature = dht.readTemperature();
-  Serial.println(temperature);
-  return String(temperature);
+  return String(temperature, 1);
+}
+
+String getHumidity() {
+  delay(dht.getMinimumSamplingPeriod());
+  float temperature = dht.readHumidity();
+  return String(temperature, 1);
 }
 
 String processor(const String& var) {
@@ -62,30 +63,27 @@ String processor(const String& var) {
   else if (var == "TEMPERATURE"){
     return getTemperature();
   }
+  else if (var == "HUMIDITY"){
+    return getHumidity();
+  }
   return String();
 }
+// -----------------------------------------
 
-void setup()
-{
-  Serial.begin(115200);            // Запуск последовательной связи
 
-  if(!LittleFS.begin()){
-    Serial.println("Error while mounting LittleFS");
-    return;
-  }
-  
-  WiFi.softAP(ssid, password);     // WiFi.softAP используется для запуска режима AP NodeMCU.
-  Serial.print("Access Point:");   // Выводим информацию через последовательный монитор
-  Serial.println(ssid);            // Сообщаем пользователю имя WiFi, установленное NodeMCU
-  Serial.print("IP-адрес:");       // И IP-адрес NodeMCU
-  Serial.println(WiFi.softAPIP()); // IP-адрес NodeMCU можно получить, вызвав WiFi.softAPIP ()
+void setup() {
+  Serial.begin(115200);
 
+  // Старт датчика DHT22 ----------------------------------------------
   pinMode(DHT_PIN, INPUT);         // D5 пин в режиме входа 
   pinMode (DHT_VCC, OUTPUT);       // D6 пин в режиме выхода 
   digitalWrite (DHT_VCC, HIGH);    // подать напряжение 3,3V на D6 пин
-  dht.begin();                     // инициализация DHT22 3
+  dht.begin();
+  // -------------------------------------------------------------------
 
-  WiFi.begin(wifi_ssid, wifi_password);  // подключение к wifi сети
+  // Подключение к wifi сети --------------------------------------------
+  WiFi.begin(wifi_ssid, wifi_password);  
+  Serial.println(".");
   Serial.println("WIFI connecting");
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -98,10 +96,26 @@ void setup()
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
+  // -------------------------------------------------------------------
 
-  restclient.begin(client, url);         // инициализация http клиента
 
-  //SERVER BLOCK
+  // Точка доступа для sonoff --------------------------------------------
+  WiFi.softAP(ssid, password);     // WiFi.softAP используется для запуска режима AP NodeMCU.
+  Serial.print("Access Point:");   // Выводим информацию через последовательный монитор
+  Serial.println(ssid);            // Сообщаем пользователю имя WiFi, установленное NodeMCU
+  Serial.print("IP-адрес:");       // И IP-адрес NodeMCU
+  Serial.println(WiFi.softAPIP()); // IP-адрес NodeMCU можно получить, вызвав WiFi.softAPIP ()
+  // -------------------------------------------------------------------
+
+
+  // Проверка файловой системы -----------------------------------------
+  if(!LittleFS.begin()){
+    Serial.println("Error while mounting LittleFS");
+    return;
+  }
+  // -------------------------------------------------------------------
+
+  // Эндпойнты web сервера ----------------------------------------------
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(LittleFS, "/index.html", String(), false, processor);
   });
@@ -114,98 +128,23 @@ void setup()
     request->send(LittleFS, "index.js", "text/js");
   });
 
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "favicon.ico", "image/ico");
+  });
+
+  server.on("/get_temperature", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/plain", getTemperature().c_str());
   });
-  // server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
-  //   String inputMessage;
-  //   String inputParam;
-  //   // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
-  //   if (request->hasParam(PARAM_INPUT_1)) {
-  //     inputMessage = request->getParam(PARAM_INPUT_1)->value();
-  //     inputParam = PARAM_INPUT_1;
-  //   }
-  //   // GET input2 value on <ESP_IP>/get?input2=<inputMessage>
-  //   else if (request->hasParam(PARAM_INPUT_2)) {
-  //     inputMessage = request->getParam(PARAM_INPUT_2)->value();
-  //     inputParam = PARAM_INPUT_2;
-  //   }
-  //   // GET input3 value on <ESP_IP>/get?input3=<inputMessage>
-  //   else if (request->hasParam(PARAM_INPUT_3)) {
-  //     inputMessage = request->getParam(PARAM_INPUT_3)->value();
-  //     inputParam = PARAM_INPUT_3;
-  //   }
-  //   else {
-  //     inputMessage = "No message sent";
-  //     inputParam = "none";
-  //   }
-  //   Serial.println(inputMessage);
-  //   request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" 
-  //                                    + inputParam + ") with value: " + inputMessage +
-  //                                    "<br><a href=\"/\">Return to Home Page</a>");
-  // });
+
+  server.on("/get_humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", getHumidity().c_str());
+  });
+
   server.onNotFound(notFound);
   server.begin();
-  //END SERVER BLOCK
-
-  delay(1000);                           // для стабильности 
 }
+ 
 
-void close_valve () {
-  // отправить запрос на включение реле
-  restclient.addHeader("Content-Type", "application/json");                                  // header
-  String RequestData = "{\"deviceid\":\"1000b91ec6\",\"data\":{\"switch\": \"on\"}}"; // payload
-  int ResponseStatusCode = restclient.POST(RequestData);                                     // post запрос
-  if (ResponseStatusCode == 200)
-    {
-      Serial.print("Relay has been switched on! Status: ");
-      Serial.println(ResponseStatusCode);
-      Serial.println("Valve has been closed: ");
-      valve_is_opened = false;
-    }
-    else
-    {
-      Serial.print("Error! HTTP Response code: ");
-      Serial.println(ResponseStatusCode);
-    }
+void loop() {
+
 }
-
-void open_valve () {
-  // отправить запрос на выключение реле
-  restclient.addHeader("Content-Type", "application/json");                                  // header
-  String RequestData = "{\"deviceid\":\"1000b91ec6\",\"data\":{\"switch\": \"off\"}}"; // payload
-  int ResponseStatusCode = restclient.POST(RequestData);                                     // post запрос
-  if (ResponseStatusCode == 200)
-    {
-      Serial.print("Relay has been switched off! Status: ");
-      Serial.println(ResponseStatusCode);
-      Serial.println("Valve has been opened: ");
-      valve_is_opened = true;
-    }
-    else
-    {
-      Serial.print("Error! HTTP Response code: ");
-      Serial.println(ResponseStatusCode);
-    }
-}
-
-void loop()
-{
-  if ((millis() - lastTime) > timerDelay)                                              // задержка
-  {
-    float temperature = dht.readTemperature();                                         // считать температуру
-    Serial.print("Temperature: "); Serial.println(temperature);
-
-    if (temperature > max_temp && valve_is_opened) {
-        close_valve();
-    } 
-    if (temperature < min_temp && !valve_is_opened) {
-        open_valve();
-    }
-
-    restclient.end(); // Free resources
-    lastTime = millis();
-  }
-}
-
-
