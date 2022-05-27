dd8b518d52dd8631adf908c781d116a9935a3506
Doszhan <doszhan.m@mail.ru>
2022-05-27 21:05:13 +0600

    point

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ String wifi_password;
 
 // настройки точки доступа 
 const char *ssid = "Actuator";
-const char *password = "password";
+const char *password = "aSus2020";
 
 // url sonoff
 const char *url = "http://192.168.4.2:8081/zeroconf/switch";
